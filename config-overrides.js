12fe1d3f6306e7629c24b1e5551d5f5270a82464
Fang Chaoqun <fcqun@sina.cn>
2019-01-28 11:10:43 +0800

    :heavy_plus_sign: :sparkles: feat: use antd

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+// module.exports = function override(config, env) {
+//   // do stuff with the webpack config...
+//   return config;
+// };
+
+const { override, fixBabelImports } = require('customize-cra');
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  }),
+);
