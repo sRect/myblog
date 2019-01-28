6fee57853c512e0f31af7eeeec113699e7689b0f
Fang Chaoqun <fcqun@sina.cn>
2019-01-28 15:02:23 +0800

    :construction: feat: Header组件进行中

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
-import { Row, Col, Button } from 'antd';
-import { HeaderWraper } from './style';
+import { Row, Col, Icon, Menu } from 'antd';
+import { HeaderWraper, InHeaderWraper } from './style';
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 class Header extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      current: 'book'
+    }
+  }
+
+  handleClick = (e) => {
+    console.log(e);
+    this.setState(() => {
+      const current = e.key;
+      return {
+        current
+      }
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
         <HeaderWraper>
-          <Row>
-            <Col span={12}> <Button type="primary">Button</Button></Col>
-            <Col span={12}>col-12</Col>
-          </Row>
+          <InHeaderWraper>
+            <Row type="flex" align="middle" justify="space-between">
+              <Col span={3}>
+                <Icon type="home" style={{ fontSize: '34px', cursor: 'pointer' }} title='首页' />
+              </Col>
+              <Col span={10} offset={7} >
+                <Menu
+                  mode="horizontal"
+                  onClick={this.handleClick}
+                  selectedKeys={[this.state.current]}
+                >
+                  <Menu.Item key="book">
+                    <Icon type="book" style={{ fontSize: '16px' }} />我的文章
+                  </Menu.Item>
+                  <SubMenu title={<span className="submenu-title-wrapper"><Icon type="star" style={{ fontSize: '16px' }} />我的收藏</span>}>
+                    <MenuItemGroup title="Vue">
+                      <Menu.Item key="setting:1" disabled>Option 1</Menu.Item>
+                      <Menu.Item key="setting:2" disabled>Option 2</Menu.Item>
+                    </MenuItemGroup>
+                    <MenuItemGroup title="React">
+                      <Menu.Item key="setting:3" disabled>Option 3</Menu.Item>
+                      <Menu.Item key="setting:4" disabled>Option 4</Menu.Item>
+                    </MenuItemGroup>
+                    <MenuItemGroup title="Flutter">
+                      <Menu.Item key="setting:5" disabled>Option 5</Menu.Item>
+                      <Menu.Item key="setting:6" disabled>Option 6</Menu.Item>
+                    </MenuItemGroup>
+                  </SubMenu>
+                  <Menu.Item key="coffee">
+                    <Icon type="coffee" style={{ fontSize: '16px' }} />踩坑记录
+                  </Menu.Item>
+                </Menu>
+              </Col>
+              <Col span={4}>button</Col>
+            </Row>
+          </InHeaderWraper>
         </HeaderWraper>
-      </React.Fragment>
+      </React.Fragment >
     )
   }
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -2,10 +2,31 @@ import styled from 'styled-components';
 
 export const HeaderWraper = styled.div`
   width: 100%;
-  width: 100%;
   height: 56px;
   border-bottom: 1px solid #f0f0f0;
   position: fixed;
   top: 0;
   left: 0;
+  text-align: center;
+`
+export const InHeaderWraper = styled.div`
+  width: 1440px;
+  height: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  .ant-row-flex {
+    height: 100%;
+    ul.ant-menu {
+      border-bottom: 0;
+      li {
+        font-size: 16px;
+      }
+      > [class^="ant-menu-"],[class*=" ant-menu-"] {
+        margin-left: 5px;
+      }
+    }
+    
+  }
 `
