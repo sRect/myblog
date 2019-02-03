97cf93a81d35d93265160c79cfe5fcb5fca7b42d
srect <fcqun@sina.cn>
2019-02-03 21:05:11 +0800

    :sparkles: feat: 使用Carousel

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List } from 'antd';
+import { List, Carousel } from 'antd';
 import { MainWrapper } from './style';
 
 class Main extends React.Component {
@@ -32,6 +32,11 @@ class Main extends React.Component {
     return (
       <React.Fragment>
         <MainWrapper>
+          <Carousel autoplay speed={800} infinite={true}>
+            <div className="bannerWrap"><img src="//bpic.588ku.com//back_origin_min_pic/18/06/10/80de9910228011f002967b07ee4f3692.jpg" alt="img" /><h3>祝贺大家新年快乐</h3></div>
+            <div><h3>2</h3></div>
+            <div><h3>3</h3></div>
+          </Carousel>
           <List
             onClick={this.handleClick}
             size="default"
--- src/components/Main/style.js
+++ src/components/Main/style.js
@@ -3,5 +3,34 @@ import styled from 'styled-components';
 export const MainWrapper = styled.div`
   width: 960px;
   height: 100%;
-  margin: 0 auto;
+  margin: 10px auto;
+  .ant-carousel {
+    .bannerWrap {
+      position: relative;
+      h3 {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 1;
+      }
+    }
+    .slick-slide {
+      text-align: center;
+      height: 260px;
+      line-height: 260px;
+      background: #364d79;
+      overflow: hidden;
+      h3 {
+        font-size: 30px;
+        color: #fff;
+      }
+      img {
+        width: 100%;
+        height: 260px;
+        background-size: cover;
+
+      }
+    }
+  }
 `
