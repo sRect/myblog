94829592a17026711011d10ed23f50f313b429c4
Fang Chaoqun <fcqun@sina.cn>
2019-03-06 15:55:08 +0800

    :constructor: feat: 路由传参

--- src/pages/ExperienceRecord/index.js
+++ src/pages/ExperienceRecord/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ExperienceArticle = () => {
-  return (
-    <div>
-      <h1>踩坑记录</h1>
-    </div>
-  )
+class ExperienceArticle extends Component {
+  render() {
+    return (
+      <div>
+        <h1>踩坑记录</h1>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    console.log(this.props)
+  }
+
+
 }
 
 export default ExperienceArticle;
