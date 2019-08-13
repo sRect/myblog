c5308ac3ff250dd7ebac4fc813bb804bb6ed0c39
Fang Chaoqun <fcqun@sina.cn>
2019-08-13 18:09:31 +0800

    chore: :wrench: 1.添加gzip压缩配置 2.关闭sourceMap

--- config-overrides.js
+++ config-overrides.js
@@ -3,11 +3,29 @@
 //   return config;
 // };
 
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addWebpackPlugin } = require('customize-cra');
+const CompressionPlugin = require('compression-webpack-plugin'); // gzip压缩
+
+const rewiredMap = () => config => {
+  config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
+  return config;
+};
+
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css',
   }),
+  // 关闭mapSource https://segmentfault.com/q/1010000018123194
+  rewiredMap(),
+  addWebpackPlugin(
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  )
 );
