167396f0d5fadb925cd1e3093910a1c5770198dc
fangchaoqun <fang.chaoqun@nissei-china.cn>
2021-12-28 11:48:44 +0800

    Updates

---
+++ precache-manifest.299b7a19937dea096f5e42f79ccd75ed.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6d52ea60af0d9ba2404",
+    "url": "/myblog/static/css/0.8a80803c.chunk.css"
+  },
+  {
+    "revision": "a6d52ea60af0d9ba2404",
+    "url": "/myblog/static/js/0.a6d52ea6.chunk.js"
+  },
+  {
+    "revision": "027e20d0b834d290f5b7",
+    "url": "/myblog/static/js/main.027e20d0.chunk.js"
+  },
+  {
+    "revision": "8844223e194bc67ce5a2",
+    "url": "/myblog/static/js/2.8844223e.chunk.js"
+  },
+  {
+    "revision": "3e73fd39d7c03ef8e406",
+    "url": "/myblog/static/js/3.3e73fd39.chunk.js"
+  },
+  {
+    "revision": "c063f9f732b762f49831",
+    "url": "/myblog/static/css/4.11d9e95d.chunk.css"
+  },
+  {
+    "revision": "c063f9f732b762f49831",
+    "url": "/myblog/static/js/4.c063f9f7.chunk.js"
+  },
+  {
+    "revision": "8a7a357596822d3d89cb",
+    "url": "/myblog/static/css/5.6460f4c2.chunk.css"
+  },
+  {
+    "revision": "8a7a357596822d3d89cb",
+    "url": "/myblog/static/js/5.8a7a3575.chunk.js"
+  },
+  {
+    "revision": "68c36b763dbdc50f7300",
+    "url": "/myblog/static/css/6.6a1e0747.chunk.css"
+  },
+  {
+    "revision": "68c36b763dbdc50f7300",
+    "url": "/myblog/static/js/6.68c36b76.chunk.js"
+  },
+  {
+    "revision": "b50c6c6c0d217961de43",
+    "url": "/myblog/static/js/runtime~main.b50c6c6c.js"
+  },
+  {
+    "revision": "fd23cf1e19c3ce5fb9fd24d2a888927f",
+    "url": "/myblog/index.html"
+  }
+];
